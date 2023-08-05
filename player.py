a9312c512a1e8457a59e8c7ecf6589950849da26
Jay Spark <JustinConcepc@gmail.com>
2023-08-04 20:23:58 -0400

    Added pycache to .gitignore.

--- player.py
+++ player.py
@@ -5,8 +5,13 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.Surface((32,64))
         self.image.fill('red')
         self.rect = self.image.get_rect(topleft = pos)
+
+        # PLAYER MOVEMENT 
         self.direction = pygame.math.Vector2(0,0)
         self.speed = 8
+        self.gravity = 0.8
+        self.jump_speed = -16
+        
 
     def get_input(self):
         keys = pygame.key.get_pressed()
